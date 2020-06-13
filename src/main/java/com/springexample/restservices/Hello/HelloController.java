a2d616a6d21ef6f4fb4ef61fb586fa903fe887aa
poonam <poonam@john-jacobs.com>
2020-06-14 00:34:22 +0530

    hello world with json data

--- src/main/java/com/springexample/restservices/Hello/HelloController.java
+++ src/main/java/com/springexample/restservices/Hello/HelloController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET,path="/hello")
-	@GetMapping("/hello1")
+	//@RequestMapping(method = RequestMethod.GET,path="/hello")
+	@GetMapping("/hello")
 	public String HelloWorld() {
 		return "hello Poonam1";
+	}
+	@GetMapping("/hello1")
+	public UserDetails Hello1() {
+		return new UserDetails("poonam","singh","delhi");
 	}
 
 }
