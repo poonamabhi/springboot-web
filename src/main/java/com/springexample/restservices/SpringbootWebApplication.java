e0fd56430c67bfb9c5ed9c4b882220077195fa22
poonam <singhspoonam@gmail.com>
2020-09-17 15:45:38 +0530

    second commit

--- src/main/java/com/springexample/restservices/SpringbootWebApplication.java
+++ src/main/java/com/springexample/restservices/SpringbootWebApplication.java
@@ -2,7 +2,7 @@ package com.springexample.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//comment
 @SpringBootApplication
 public class SpringbootWebApplication {
 
